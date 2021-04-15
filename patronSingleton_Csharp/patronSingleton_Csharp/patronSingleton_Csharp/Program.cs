f128adbd4e87584bc93044dd843e37239d91f164
Hans O. Otano Cuesta <hansoliver23@gmail.com>
2021-04-15 01:11:55 -0400

    Add files via upload

---
+++ patronSingleton_Csharp/patronSingleton_Csharp/patronSingleton_Csharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patronSingleton_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 0;
+            int numero2 = 0;
+
+            //traer el objeto al main
+            objetoSingleton objeto = objetoSingleton.traerInstanciaSingle();
+            objeto.sumar_Numeros(numero1, numero2); //calcular numeros
+            Console.ReadKey();
+        }
+    }
+}
---
+++ patronSingleton_Csharp/patronSingleton_Csharp/patronSingleton_Csharp/objetoSingleton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patronSingleton_Csharp
+{
+    class objetoSingleton
+    {
+        //-- Creamos el objeto
+
+        private static objetoSingleton instanciaSingle = new objetoSingleton();
+
+
+        //Hacemos que el constructor sea privado para que no sea instanciado
+
+        private objetoSingleton() { }
+
+
+
+        //Traemos el objeto mediante un return
+        public static objetoSingleton traerInstanciaSingle()
+        {
+            return instanciaSingle;
+        }
+
+        //Funcion para sumar numeros
+        public void sumar_Numeros(int n1, int n2)
+        {
+            Console.WriteLine("Introduce el primer numero:");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduce el segundo numero:");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            n1 = n1 + n2;
+
+            Console.WriteLine("El resultado es: " + n1);
+        }
+
+    }
+}
